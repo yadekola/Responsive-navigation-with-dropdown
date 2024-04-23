b694a13f831c99c5e1bd7168a74339be0fe2602f
Adekola <yakubadekolaojo059@gmail.com>
2024-04-23 14:46:25 +0100

    first commit

---
+++ js/mobile-nav.js
@@ -0,0 +1,23 @@
+// Grabbing the header, and the "mobile toggle button"
+let headerMain = document.querySelector(".header-main");
+let headerMobileBtn = document.querySelector(".mobile-menu-btn");
+
+// Boolean to state if the menu is opened or closed
+let isMenuOpen = false;
+
+// Event to check if the "mobile toggle button" is clicked
+headerMobileBtn.onclick = function () {
+    // Condition to check if the menu is currently open or closed
+    if (!isMenuOpen) {
+        // We change the header styling to "display: flex;"
+        headerMain.style.display = "flex";
+        // We change the boolean
+        isMenuOpen = true;
+    }
+    else if (isMenuOpen) {
+        // We change the header styling to "display: none;"
+        headerMain.style.display = "none";
+        // We change the boolean
+        isMenuOpen = false;
+    }
+}
